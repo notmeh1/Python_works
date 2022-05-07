ead66dff108e11169561d6e4c5f3f64a67baa91f
Fernando <zleckzxd@gmail.com>
2022-05-07 16:38:47 -0400

    added date to cat_t3

--- CAT_T3/date.py
+++ CAT_T3/date.py
@@ -1,2 +1,38 @@
-import datetime
+from datetime import date, datetime, time
 
+print("1.- Ver fecha con formato norteamericano")
+print("2.- Ver fecha con formato sudamericano")
+print("3.- Ver Hora actual")
+print("4.- Ver fecha y hora actual")
+
+# Obtiene la fecha actual
+currentDate = date.today()
+current = datetime.now()
+
+loop = True
+while loop:
+    userInput = input("Seleccione la lista que quiera ver... ")
+    if userInput == "1":
+
+        # Se ordena la fecha al formato regional Norteamericano
+        formatedDateOne = currentDate.strftime("%B/%d/%Y")
+        print("La fecha actual es:",formatedDateOne)
+
+    elif userInput == "2":
+        # Se ordena la fecha al formato regional Sudamericano
+        formatedDateTwo = currentDate.strftime("%d/%b/%Y")
+
+        print("La fecha actual es:",formatedDateTwo)
+    elif userInput == "3":
+        # Se obtiene la fecha y hora actual
+
+        # Se ordena el tiempo a (Horas, minutos y segundos)
+        currentTime = current.strftime("%H:%M:%S")
+        print("La hora actual es:",currentTime)
+
+    elif userInput == "4":
+        # Se ordena el tiempo a (Dias, meses, años, horas, minutos y segundos)
+        currentTimeComplete = current.strftime("%d/%b/%Y"+" " +"%H:%M:%S")
+        print("La fecha y hora actual es: ",currentTimeComplete)
+    else:
+        print("El valor ingresado es invalido.")
--- CAT_T3/keyList.py
+++ CAT_T3/keyList.py
@@ -5,9 +5,11 @@ print("2.- Lista de Softwords")
 userInput = input("Seleccione la lista que quiera ver... ")
 if userInput == "1":
     print("Lista de Keywords: ")
+    # Imprime la lista de keywords
     print(keyword.kwlist)
 elif userInput == "2":
     print("List de Softwords: ")
+    # Imprime la lista de softwords
     print(keyword.softkwlist)
 else:
     print("El valor ingresado es invalido.")
